5e573430afe974e6907aa595f594f5e07655dc42
LuckyIYI <lacoz@icloud.com>
2025-07-22 22:13:07 -0700

    Initial Commit

---
+++ MetalTensorOp/MetalTensorOpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalTensorOpApp.swift
+//  MetalTensorOp
+//
+//  Created by Лаки Ийнбор on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MetalTensorOpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
